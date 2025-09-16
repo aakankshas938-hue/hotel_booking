a1c7ffc03a3e230859ece14ad2a66fe98feec60e
aakankshas938-hue <aakankshas938@gmail.com>
2025-09-16 20:24:58 +0530

    Fix wsgi settings

--- hotel_project/wsgi.py
+++ hotel_project/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Correct path to your settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_project.settings')
 
 application = get_wsgi_application()
